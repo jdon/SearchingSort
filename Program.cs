6c8d1ae59f03e084b5dcaa2c3ca62595edc13197
Jonathan Donaldson <jdonaldson278@gmail.com>
2016-03-29 23:19:17 +0100

    Updated Menu

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace SearchingSort
 {
     class Program
     {
+        private static FileManager fm = new FileManager();
+
         static void Main(string[] args)
         {
             string selection = "0";
@@ -31,7 +33,7 @@ namespace SearchingSort
                         }
                         else
                         {
-                            DisplayMainMenu(fm);
+                            DisplaySearchorSortMenu();
                         }
                         break;
                     case "2":
@@ -48,51 +50,79 @@ namespace SearchingSort
         {
             Console.WriteLine("Please enter the directory of the files\n");
             String dir = Console.ReadLine();
-            FileManager fm = new FileManager();
             Boolean FilesCorrect = fm.inputFiles(@dir);
             while (!FilesCorrect)
             {
                 //input not valid
                 Console.WriteLine("Invalid input, Please enter the directory of the files");
-                Console.WriteLine("Enter 3 to quit\n");
+                Console.WriteLine("Type exit to return to the previous menu\n");
                 dir = Console.ReadLine();
-                if (dir == "3") return;
+                if (dir == "exit") return;
                 FilesCorrect = fm.inputFiles(@dir);
             }
             //correct files found and are valid
-            DisplayMainMenu(fm);
+            DisplaySearchorSortMenu();
         }
 
-        private static void DisplayMainMenu(FileManager fm)
+        private static void DisplaySearchorSortMenu()
         {
-            Console.WriteLine("Main Menu\n");
+            Console.WriteLine("Select if you would like to search or sort\n");
             string selection = "0";
             do
             {
-                Console.WriteLine("1. Sort by year ascending");
-                Console.WriteLine("2. Sort by year decending");
-                Console.WriteLine("3. Quit");
+                Console.WriteLine("1. Search");
+                Console.WriteLine("2. Sort - Ascending");
+                Console.WriteLine("3. Sort - descending");
+                Console.WriteLine("4. Quit");
                 selection = Console.ReadLine();
 
                 switch (selection)
                 {
                     case "1":
-                        // sort and display year by ascending
-                        fm.sort(false);
-                        foreach (WeatherData wd in fm.WeatherData1)
-                        {
-                            Console.WriteLine(wd.ToString());
-                        }
+                        // display search menu
+                        int i = fm.BinarySearchYears(1950);
+                        Console.WriteLine(i);
+                        //TODO
                         break;
                     case "2":
-                        // sort and display year by ascending
-
+                        // display sort menu
+                        DisplayMainMenu(true);
+                        break;
+                    case "3":
+                        // display sort menu
+                        DisplayMainMenu(false);
                         break;
                     default:
                         break;
                         // etc..
                 }
-            } while (selection != "3");
+            } while (selection != "4");
+        }
+
+        private static void DisplayMainMenu(Boolean isAscending)
+        {
+            string selection = "0";
+            do
+            {
+                Console.WriteLine("Please type in which of the following fields you would like to sort\n");
+                Console.WriteLine("Year, Month, WS1_AF, WS1_Rain, WS1_Sun, WS1_TMax, WS1_Tmin, WS2_AF, WS2_Rain, WS2_Sun, WS2_TMax, WS2_Tmin");
+                Console.WriteLine("Type exit to return to the previous menu\n");
+                selection = Console.ReadLine();
+                try
+                {
+                    fm.QuickSort_custom(isAscending, selection);
+                }
+                catch (Exception e)
+                {
+                    if (selection == "exit") break;
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+                foreach (WeatherData wd in fm.WeatherDataArray1)
+                {
+                    Console.WriteLine(wd.ToString());
+                }
+            } while (selection != "exit");
         }
     }
 }
